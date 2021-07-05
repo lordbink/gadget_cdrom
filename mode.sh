61e10f7985db7e545eb7ede46317049f983ddbeb
Richard Anderson <lordbink@gmail.com>
2021-07-05 14:28:48 -0500

    missing reversion

--- mode.sh
+++ mode.sh
@@ -8,7 +8,7 @@ MODE=${1:-hdd}
 source "$IDIR/clean.sh"
 
 if [ "$MODE" == "hdd" ] ; then
-    modprobe g_mass_storage file=/dev/mmcblk0p3 luns=1 stall=0 ro=0 cdrom=0 removable=1
+    modprobe g_mass_storage file=/iso.img luns=1 stall=0 ro=0 cdrom=0 removable=1
 elif [ "$MODE" == "cd" ] ; then
      mount -o ro "$(losetup -PLf /iso.img --show)p1" /iso
 elif [ "$MODE" == "usb" ] ; then
